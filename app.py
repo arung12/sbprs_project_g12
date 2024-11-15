4a0a32aae7754be16703518eb107a73f20f5ac37
Arunkumar Gunasekaran <manigunasekaran30@gmail.com>
2024-11-16 00:42:41 +0530

    model with recommendations

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import model 
 
 app = Flask(__name__)
 
@@ -17,48 +18,7 @@ def recommend_top_5_products():
     return render_template("index.html", users=users,recommended_products=recommended_products)
 
 def recommend_products(username):
-    # Simple mock recommendation based on username length for illustration
-    products  = [
-            {
-                'name': 'Product 1',
-                'description': 'A great product for everyone.',
-                'price': '99.99',
-                'url': '/product/1',
-            },
-            {
-                'name': 'Product 2',
-                'description': 'An amazing new product.',
-                'price': '49.99',
-                'url': '/product/2',
-            },
-            {
-                'name': 'Product 1',
-                'description': 'A great product for everyone.',
-                'price': '99.99',
-                'url': '/product/1',
-            },
-            {
-                'name': 'Product 2',
-                'description': 'An amazing new product.',
-                'price': '49.99',
-                'url': '/product/2',
-            },
-            {
-                'name': 'Product 1',
-                'description': 'A great product for everyone.',
-                'price': '99.99',
-                'url': '/product/1',
-            },
-            {
-                'name': 'Product 2',
-                'description': 'An amazing new product.',
-                'price': '49.99',
-                'url': '/product/2',
-            }
-            # Add more products as needed
-        ]
-    # In a real case, this could be a model prediction or product filtering logic.
-    recommended = products #[:5]  # Recommend first 5 products as an example
+    recommended = model.recommend_products(username)
     return recommended
 
 if __name__ == "__main__":
---
+++ model.py
@@ -0,0 +1,78 @@
+import os
+import pickle
+
+import pandas as pd
+import numpy as np
+import spacy
+import re
+
+import nltk
+from nltk.tokenize import word_tokenize
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+nlp = spacy.load('en_core_web_sm',  disable=["parser", "ner"])
+
+filePath = ''
+
+def load_from_pickle(path):
+   if os.path.exists(path):
+       return pickle.load(open(path,'rb'))
+   else:
+       return None
+
+tfidf_vectorizer = load_from_pickle(f'{filePath}/pickle_files/tfidf_vectorizer.pkl')
+model = load_from_pickle(f'{filePath}/pickle_files/model_final.pkl')
+recommendation_matrix = load_from_pickle(f'{filePath}/pickle_files/user_final_rating.pkl','rb')
+products_data = load_from_pickle(f'{filePath}/pickle_files/products_data.pkl','rb')
+
+def pre_process_text(text):
+    #converting text to lower
+    text = text.lower()
+
+    #removing any punctuation character by selecting anything other than word and space
+    text = re.sub(r'[^\w\s]',"",text)
+
+    #removing the promotion message
+    text = re.sub(r'This review was collected as part of a promotion', '', text)
+    
+    #removing stop words and extrating lemma
+    tokens = nlp(text)
+    text = [token.lemma_ for token in tokens if not token.is_stop]
+
+    #removing spaces at the front and end
+    text = ' '.join(text)
+    text = text.strip()
+
+    return text
+
+def get_recommended_products(username):
+    recommendations = recommendation_matrix.loc[user_input].sort_values(ascending=False)[0:20]
+    top20_recommendations = pd.DataFrame({'id': recommendations.index, 'cosine_similarity_score' : recommendations})
+    top20_recommended_products = products_data[products_data['id'].isin(top20_recommendations['id'])]
+    top20_recommended_products = top20_recommended_products[['id','name','combined_review']]
+    top20_recommended_products = top20_recommended_products.drop_duplicates()
+
+    X = tfidf_vectorizer.transform(top20_recommended_products['combined_review'])
+    top20_recommended_products['predicted_sentiment'] = model.predict(X)
+
+    grouped_df = top20_recommended_products.groupby(['id'])['predicted_sentiment'].agg(['sum', 'count'])
+    grouped_df['positive_percentage'] = grouped_df['sum'] / grouped_df['count'] * 100
+    top5_recommendations = grouped_df.sort_values(by='positive_percentage', ascending=False).head(5)
+    print("*"*50)
+    print(tabulate(top5_recommendations,headers="keys", tablefmt="grid"))
+    top5_recommended_products = products_data[products_data['id'].isin(top5_recommendations.index)]
+    top5_recommended_products = top5_recommended_products[['id','name','brand','categories',	'manufacturer']]
+    top5_recommended_products = top5_recommended_products.drop_duplicates()
+
+    recommendations_final = pd.merge(top5_recommended_products,top5_recommendations,left_on='id',right_on='id', how = 'left')
+    recommendations_final = recommendations_final[['id','name','brand','categories',	'manufacturer','positive_percentage']].sort_values(by='positive_percentage', ascending=False)
+    return recommendations_final
